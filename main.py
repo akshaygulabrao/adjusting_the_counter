023c0b307c7f116374095fc7e1d8e25b0ca0d6a3
Akshay Gulabrao <agula002@ucr.edu>
2022-05-09 00:35:11 -0700

    bfs done, but computationally intractable

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+# https://stackoverflow.com/questions/47004506/check-if-a-numpy-array-is-sorted
+# https://www.dropbox.com/sh/ftzvcnntl2j5eiu/AAASovmq047jOClaHjL4h4GIa?dl=0&preview=2__Blind+Search_part1.pptx
+import numpy as np
+import queue
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def goal_test(board):
+    return np.all(board[:-1] <= board[1:])
+# computationally intractable without heuristics
+def bfs(initial):
+    # Each state is a 2-tuple (1) board state (2) list of swaps
+    frontier = queue.Queue()
+    frontier.put((tuple(initial),()))
+    explored = set()
+    while(True):
+        if frontier.empty(): return 'failure'
+        curr,swaps = frontier.get()
+        ops = list(swaps)
+        if goal_test(np.array(curr)): return curr,swaps
+        c = np.array(curr)
+        for swap1 in range(len(c)):
+            for swap2 in range(swap1,len(c)):
+                c[swap1],c[swap2] = c[swap2],c[swap1]
+                if tuple(c) not in explored:
+                    explored.add(tuple(c))
+                    ops.append((swap1,swap2))
+                    frontier.put((tuple(c),tuple(ops)))
+
+a = np.arange(25)
+np.random.shuffle(a)
+print(bfs(a))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
